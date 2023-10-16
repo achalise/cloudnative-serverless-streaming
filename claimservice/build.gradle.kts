2892cffd8fd142c7d2ce23f1134a324fdc0a4a6a
Arun Chalise <chalise.arun@gmail.com>
2023-10-16 22:18:24 +1100

    build and application properties for aws lambda deployment

--- claimservice/build.gradle.kts
+++ claimservice/build.gradle.kts
@@ -1,9 +1,9 @@
-//import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-//import com.github.jengelman.gradle.plugins.shadow.transformers.PropertiesFileTransformer
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import com.github.jengelman.gradle.plugins.shadow.transformers.PropertiesFileTransformer
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	//id("com.github.johnrengelman.shadow") version "8.1.1"
+	id("com.github.johnrengelman.shadow") version "8.1.1"
 	id("org.springframework.boot") version "3.1.4"
 	id("io.spring.dependency-management") version "1.1.3"
 	kotlin("jvm") version "1.8.22"
@@ -28,8 +28,8 @@ dependencies {
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 	implementation("org.springframework.cloud:spring-cloud-function-web")
 	implementation ("org.springframework.kafka:spring-kafka")
-	//implementation("org.springframework.cloud:spring-cloud-function-adapter-aws")
-	//implementation("org.springframework.boot.experimental:spring-boot-thin-gradle-plugin:1.0.31.RELEASE")
+	implementation("org.springframework.cloud:spring-cloud-function-adapter-aws")
+	implementation("org.springframework.boot.experimental:spring-boot-thin-gradle-plugin:1.0.31.RELEASE")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
 	testImplementation ("org.springframework.kafka:spring-kafka-test")
@@ -53,28 +53,30 @@ tasks.withType<Test> {
 	useJUnitPlatform()
 }
 
-//tasks.assemble{
-//	dependsOn("shadowJar")
-//}
-//
-//tasks.withType<ShadowJar> {
-//    archiveClassifier.set("aws")
-//	dependencies {
-//		exclude("org.springframework.cloud:spring-cloud-function-web")
-//	}
-//	// Required for Spring
-//	mergeServiceFiles()
-//	append("META-INF/spring.handlers")
-//	append("META-INF/spring.schemas")
-//	append("META-INF/spring.tooling")
-//	transform(PropertiesFileTransformer::class.java) {
-//		paths.add("META-INF/spring.factories")
-//		mergeStrategy = "append"
-//	}
-//}
-//
-//tasks.withType<Jar> {
-//	manifest {
-//		attributes["Start-Class"] = "com.arun.claimservice.ClaimserviceApplication"
-//	}
-//}
+tasks.assemble{
+	dependsOn("shadowJar")
+}
+
+tasks.withType<ShadowJar> {
+    archiveClassifier.set("aws")
+	dependencies {
+		exclude("org.springframework.cloud:spring-cloud-function-web")
+	}
+	// Required for Spring
+	mergeServiceFiles()
+	append("META-INF/spring.handlers")
+	append("META-INF/spring.schemas")
+	append("META-INF/spring.tooling")
+	append("META-INF/spring/org.springframework.boot.autoconfigure.AutoConfiguration.imports")
+	append("META-INF/spring/org.springframework.boot.actuate.autoconfigure.web.ManagementContextConfiguration.imports")
+	transform(PropertiesFileTransformer::class.java) {
+		paths.add("META-INF/spring.factories")
+		mergeStrategy = "append"
+	}
+}
+
+tasks.withType<Jar> {
+	manifest {
+		attributes["Start-Class"] = "com.arun.claimservice.ClaimserviceApplication"
+	}
+}
